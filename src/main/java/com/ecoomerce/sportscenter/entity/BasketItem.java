c57903eab7053ee7f021289b796b9b2544bd845b
Shraddha Bhivraj Deshmukh <shraddhabdeshmukh2110@gmail.com>
2026-04-15 23:29:02 +0530

    Change price field type from Long to Double
    Changed the price field type from Long to Double for better precision.

--- src/main/java/com/ecoomerce/sportscenter/entity/BasketItem.java
+++ src/main/java/com/ecoomerce/sportscenter/entity/BasketItem.java
@@ -1,16 +1,21 @@
 package com.ecoomerce.sportscenter.entity;
-import lombok.Data;
+
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @RedisHash("BasketItem")
 public class BasketItem {
+
     @Id
     private Integer id;
+
     private String name;
     private String description;
-    private Long price;
+    private Double price;   // 🔥 changed (see below)
     private String pictureUrl;
     private String productBrand;
     private String productType;
