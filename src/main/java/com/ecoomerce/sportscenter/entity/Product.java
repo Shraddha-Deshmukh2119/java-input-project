f9cde45e5cd83ec807e75104ebe7f7b63fdfbe02
Shraddha Bhivraj Deshmukh <shraddhabdeshmukh2110@gmail.com>
2026-04-15 23:30:44 +0530

    Update Product.java

--- src/main/java/com/ecoomerce/sportscenter/entity/Product.java
+++ src/main/java/com/ecoomerce/sportscenter/entity/Product.java
@@ -1,36 +1,36 @@
 package com.ecoomerce.sportscenter.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Table(name="Product")
+@Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="Id")
-    Integer id;
-    @Column(name="Name")
+    private Integer id;
+
     private String name;
-    @Column(name="Description")
+
     private String description;
-    @Column(name="Price")
-    private Long price;
-    @Column(name="PictureUrl")
+
+    private Double price;
+
     private String pictureUrl;
 
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name= "ProductBrandId", referencedColumnName = "Id")
+    @JoinColumn(name = "product_brand_id")
     private Brand brand;
 
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name= "ProductTypeId", referencedColumnName = "Id")
+    @JoinColumn(name = "product_type_id")
     private Type type;
 }
